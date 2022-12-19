c64c85c4cf701f4890dc6d02acba104de3dee847
Jean-Colli <110646063+Jean-Colli@users.noreply.github.com>
2022-12-18 22:45:56 -0300

    retirando classe inutil

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import { IButtonProps } from "./types";
 
 const Button = ({ title, onClick, disabled }: IButtonProps) => {
   if (disabled){
-    return <ButtonContainer className="hover" onClick={onClick} >{title}</ButtonContainer>;
+    return <ButtonContainer onClick={onClick} >{title}</ButtonContainer>;
   }else{
     return <ButtonContainerDisabled disabled>{title}</ButtonContainerDisabled>;
   }
